cbfa0ffab454b389848dad78317c0fc42d85bde6
Swapna Jha <45579175+SJ-tech13@users.noreply.github.com>
2023-07-18 05:32:03 -0400

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,14 @@
+
+public class Contact {
+
+    public Contact(String name, int age, int number, int mon, int day) {
+    }
+
+    public Object getName() {
+        return null;
+    }
+
+    public void printContact() {
+    }
+
+}
